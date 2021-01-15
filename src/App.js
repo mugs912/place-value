dc550b639e6c369f92d5b6864b1764f21202dfbe
admin <mugdhavdeshmukh1998@gmail.com>
2021-01-15 22:26:11 +0530

    added next button

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ export default function App() {
   return (
     <div className="App">
         <GenerateNumber/>
+        <div className='btn-div'>
+          <button className='btn' onClick={() => {window.location.reload(false)} }>Next Question</button>
+        </div>
     </div>
   );
 
